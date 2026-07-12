3130354df7b9f3f03229661e42ba7ea57634333d
PrinceRitik <hritikraj66778@gmail.com>
2026-07-12 13:57:38 +0530

    refactor: Update CORS configuration to use allowedOrigins array for better maintainability

--- server/server.js
+++ server/server.js
@@ -16,10 +16,23 @@ const app = express();
 await connectDB();
 
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL,
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(clerkMiddleware())
 
 app.get('/', (req,res)=> {
